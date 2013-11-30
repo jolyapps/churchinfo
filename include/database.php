8c3e5814772db1930ca46aee22efafb863980801
engrsam <engrsam@users.noreply.github.com>
2013-11-29 16:01:53 -0800

    Changed sensitive database connection info to AWS RDS

--- include/database.php
+++ include/database.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER', 'churchinfo.cczwts500sen.eu-west-1.rds.amazonaws.com:3306');
+define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'dclmict4Christ');
+define('DB_PASSWORD', '');
 define('DB_DATABASE', 'locprofile');
 
 $con = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die(mysql_error());
